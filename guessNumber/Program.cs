5b12547d79e118eb01e8f28267b0866dc9ac52eb
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-05 19:02:10 +0100

    Guess number

---
+++ guessNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Guess number, 6 tries
+            int tries = 0;
+            bool correct = false;
+            int guess = 0;
+            Random rnd = new Random();
+            int randNum = rnd.Next(1, 101);
+            while (tries < 6 && !correct)
+            {
+                Console.WriteLine("Enter your guess:");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == randNum)
+                {
+                    Console.WriteLine($"Correct the number was {randNum}");
+                    correct = true;
+                    break;
+                }
+                tries++;
+                //Console.WriteLine($"Incorrect! {guess} is not the number, you have {6 - tries} tries remaining");
+                if (guess < randNum)
+                {
+                    Console.WriteLine($"Too low! {guess} is not the number, you have {6 - tries} tries remaining");
+                }
+                else
+                {
+                    Console.WriteLine($"Too high! {guess} is not the number, you have {6 - tries} tries remaining");
+                }
+            }
+            if (!correct)
+            {
+                Console.WriteLine($"Unlucky! The number was {randNum}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
